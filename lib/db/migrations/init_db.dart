d42890de4c6a135954b0e2999f081cd7fa443c24
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-03-22 14:06:44 +0630

    add auto increment to id in category table schema

--- lib/db/migrations/init_db.dart
+++ lib/db/migrations/init_db.dart
@@ -1,6 +1,6 @@
 const String initDbScript = """
   CREATE TABLE Category(
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     description TEXT,
     iconCodePoint INTEGER
